b7fc222b85af110390d29e09e44853a117c8d9e8
Francesco <ambrogifra@gmail.com>
2020-04-24 14:19:17 -0400

    Fixed the Newton Raphson method

--- NR_method.py
+++ NR_method.py
@@ -5,11 +5,13 @@
 # x_new = x - f(x) / f'(x)
 # Therefore we need to evaluate the derivative of the f
 # let us use thea same equation as simple_it_method
-# Let's solve equation 3x**2 -5x +2 = 0
+
+# Let's solve first equation 3x**2 -5x +2 = 0
 # Analytical solutions: x_1 = 1, x_2 = 2/3 
 '''
 import time
 from matplotlib import pyplot as plt
+from math import sin, cos
 
 x = 0 # initial guess
 eps = 10e-06 # tolerance
@@ -30,9 +32,44 @@ for it in range(1,101): # Where it is the counter for iterations
         break
     x = x_new
     
-plt.show()
-    
 print('Number of iterations: %d' % it)
 print('The solution found is: %0.5f' % x_new)
-print('The calculation with for took: ' + str('{0:2f}'.format(time.time() -t)) + 's\n')    
+print('The calculation took: ' + str('{0:2f}'.format(time.time() -t)) + 's\n') 
+
+"""
+Let's now solve equation
+f(y) = y**2 + cos**2(y) - 4y
+f'(y) = 2y - 2*sin(y)cos(y) - 4
+"""
+
+y = 10 # initial guess
+t1 = time.time()
+plt.figure(21,figsize=(22,14), dpi=100)
+plt.grid(True)
+
+for it in range(1,101): # Where it is the counter for iterations
+    y_new = y - (y**2 + cos(y)**2 - 4*y)/(2*(y -sin(y)*cos(y) - 2))
+    
+    plt.plot(it, y_new, '-ko')
+    plt.plot(it, y, '-ro')
+    plt.pause(0.05)
+    plt.xlabel('Iterations', fontsize=20)
+    plt.ylabel('Value of y', fontsize=20)
+    # Check for convergence
+    if abs(y_new - y) < eps:
+        break
+    y = y_new
+
+print('Number of iterations: %d' % it)
+print('The solution found is: %0.5f' % y_new)
+print('The calculation took: ' + str('{0:2f}'.format(time.time() -t1)) + 's\n') 
+    
+    
+
+
+    
+    
+plt.show()
+    
+   
     
