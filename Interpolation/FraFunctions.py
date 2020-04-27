6c2381eb7e9af51189e61a534e0e507badce7ed8
Francesco <ambrogifra@gmail.com>
2020-04-27 11:51:05 -0400

    Updated the FraFunction files

--- Interpolation/FraFunctions.py
+++ Interpolation/FraFunctions.py
@@ -3,14 +3,17 @@ In this file we define useful functions
 """
 from matplotlib import pyplot as plt
 
-# the first function is a 2D plot function
-def plot2D(x , y, fname=None, xlabel=None, ylabel=None ,**kwargs):   
+# The first function is a 2D plot function
+def plot1D(x , y, fname=None, label=None, xlabel=None, ylabel=None ,**kwargs):   
     "This function will build a 2D plot and it will save it if fname is not true."
     plt.figure(101, figsize=(22,14), dpi=100)
-    plt.plot(x, y, **kwargs)
-    plt.grid(True)
+    plt.plot(x, y,label=label, **kwargs)
+    plt.grid(False)
     plt.rc('xtick', labelsize=32) 
     plt.rc('ytick', labelsize=32)
+    
+    if label:
+        plt.legend(loc='upper left', frameon=False, fontsize=22)
     
     if xlabel and ylabel:
         plt.xlabel(xlabel, fontsize=32)
