6703878947dc2564a5713f3f5e3ca1c8a76336a2
Francesco <ambrogifra@gmail.com>
2020-11-13 09:13:04 -0500

    Minor changes to euler method for ODE

--- ODEsolve/eulerMethod.py
+++ ODEsolve/eulerMethod.py
@@ -12,17 +12,22 @@ import numpy as np
 
 # definition of the function
 dy = lambda x,y: x*y
+
 # exact function
 xex = np.linspace(0,2,100)
 yex = np.exp(xex**2/2)
+
 # x domain
 x = 0
 xn = 2
 y = 1 # initial value
 h = 0.5 # step size
+
 n = int((xn - x) / h)
+
 print('x \t\t y')
 print('%f \t %f' % (x,y))
+
 for i in range(n):
     y = y + dy(x,y)*h 
     x = x + h
